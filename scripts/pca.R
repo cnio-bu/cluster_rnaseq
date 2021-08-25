39e817669b57219d31a64773b20c8834aac18fd9
Maria Jose Jimenez-Santos <mj.jimenezsantos@outlook.com>
2021-08-25 12:49:38 +0200

    Make pca colors consistent accross plots

--- scripts/pca.R
+++ scripts/pca.R
@@ -3,6 +3,7 @@ sink(log)
 sink(log, type = "message")
 
 suppressMessages(library("DESeq2"))
+suppressMessages(library("scales"))
 suppressMessages(library("ggplot2"))
 suppressMessages(library("ggrepel"))
 suppressMessages(library("patchwork"))
@@ -18,16 +19,28 @@ levels <- snakemake@params[["levels"]]
 # Get vst
 vsd <- readRDS(vsd)
 
+# Assign a color to each level
+all_levels <- levels(colData(vsd)$condition)
+colors <- setNames(hue_pal()(length(all_levels)), all_levels)
+
+# Subset the colors according to the specified levels
+colors <- colors[levels]
+
 # Subset the sample names according to the specified levels
 samples <- rownames(colData(vsd))[colData(vsd)$condition %in% levels]
 
 # Subset samples
 vsd <- vsd[, samples]
 
+# Relevel levels to keep original order
+
 # PCA plots for the first 3 principal components
-pca12 <- plotPCA.3(vsd)
-pca13 <- plotPCA.3(vsd, pc = c(1, 3))
-pca23 <- plotPCA.3(vsd, pc = c(2, 3))
+pca12 <- plotPCA.3(vsd) + 
+  scale_color_manual(values = colors, breaks = all_levels)
+pca13 <- plotPCA.3(vsd, pc = c(1, 3)) + 
+  scale_color_manual(values = colors, breaks = all_levels)
+pca23 <- plotPCA.3(vsd, pc = c(2, 3)) + 
+  scale_color_manual(values = colors, breaks = all_levels)
 
 # Make patchwork with the three plots
 pca <- pca12 + pca13 + pca23 + 
