01b54546d170af0047bbf8a5f55560332dafaa3a
Maria Jose Jimenez-Santos <mj.jimenezsantos@outlook.com>
2021-08-25 16:13:32 +0200

    Plot PCAs with more than one variable

--- scripts/pca.R
+++ scripts/pca.R
@@ -3,6 +3,9 @@ sink(log)
 sink(log, type = "message")
 
 suppressMessages(library("DESeq2"))
+suppressMessages(library("stringr"))
+suppressMessages(library("tidyr"))
+suppressMessages(library("dplyr"))
 suppressMessages(library("scales"))
 suppressMessages(library("ggplot2"))
 suppressMessages(library("ggrepel"))
@@ -14,32 +17,46 @@ vsd <- snakemake@input[["vst"]]
 
 ## SNAKEMAKE PARAMS ##
 levels <- snakemake@params[["levels"]]
+design <- snakemake@params[["design"]]
 
 ## CODE ##
 # Get vst
 vsd <- readRDS(vsd)
 
+# Get variables to plot by from design
+variables <- unlist(str_split(design, pattern = "\\*|:|\\+"))
+variables <- str_trim(str_remove(variables, "~"))
+
+# colData
+coldata <- as.data.frame(colData(vsd))
+
+# Get variables' all combined levels
+all_levels <- coldata %>% unite("combined", all_of(variables), sep = ":") %>% 
+  select(combined) %>% pull %>% unique
+
 # Assign a color to each level
-all_levels <- levels(colData(vsd)$condition)
 colors <- setNames(hue_pal()(length(all_levels)), all_levels)
+
+# Subset the sample names according to the specified levels
+coldata <- coldata %>% filter(get(variables[1]) %in% levels)
+samples <- rownames(coldata)
+
+# Keep only specified levels
+levels <- coldata %>% unite("combined", all_of(variables), sep = ":") %>% 
+  select(combined) %>% pull %>% unique
 
 # Subset the colors according to the specified levels
 colors <- colors[levels]
 
-# Subset the sample names according to the specified levels
-samples <- rownames(colData(vsd))[colData(vsd)$condition %in% levels]
-
 # Subset samples
 vsd <- vsd[, samples]
 
-# Relevel levels to keep original order
-
 # PCA plots for the first 3 principal components
-pca12 <- plotPCA.3(vsd) + 
+pca12 <- plotPCA.3(vsd, intgroup = variables) + 
   scale_color_manual(values = colors, breaks = all_levels)
-pca13 <- plotPCA.3(vsd, pc = c(1, 3)) + 
+pca13 <- plotPCA.3(vsd, intgroup = variables, pc = c(1, 3)) + 
   scale_color_manual(values = colors, breaks = all_levels)
-pca23 <- plotPCA.3(vsd, pc = c(2, 3)) + 
+pca23 <- plotPCA.3(vsd, intgroup = variables, pc = c(2, 3)) + 
   scale_color_manual(values = colors, breaks = all_levels)
 
 # Make patchwork with the three plots
